1308a3008f28c8ab7ea8a3bd3194d28966e46cc9
arodrigoca <adri_rodri_64@hotmail.es>
2017-12-27 01:14:20 +0100

    more work done

--- uaclient.py
+++ uaclient.py
@@ -15,26 +15,47 @@ tags = {'account':['username', 'passwd'],
 'audio':['path']
 }
 
+sipmethods = ['REGISTER',
+'INVITE',
+'ACK',
+'BYE'
+]
 
-def doClient(server_addr, sipmsg):
+sipresponses = ['100',
+'190',
+'200'
+]
+
+def composeSipMsg(method, address):
+    """composeSipMsg creates a good formatted SIP message.
+
+    Arguments needed are (method, address)
+
+    """
+    sipmsg = method + " " + "sip:" + address[2] + '@' + address[0] \
+        + ' ' + "SIP/2.0\r\n\r\n"
+
+    return sipmsg
+
+def doClient(config_data, sip_method):
     """Main function of the program. It does server-client communication.
 
     Arguments needed are (server_addr, sipmsg)
     """
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         try:
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            my_socket.connect((server_addr[0], server_addr[1]))
-            LINE = composeSipMsg(sipmsg, server_addr)
+            my_socket.connect((config_data['regproxy']['ip'],
+            int(config_data['regproxy']['port'])))
+            LINE = composeSipMsg()
             print("Sending: " + LINE)
             my_socket.send(bytes(LINE, 'utf-8'))
             while True:
                 data = my_socket.recv(1024)
                 if data:
                     print('received -- ', data.decode('utf-8'))
-                    okline = 'SIP/2.0 100 Trying\r\n\r\n'
-                    okline = okline + 'SIP/2.0 180 Ringing\r\n\r\n'
-                    okline = okline + 'SIP/2.0 200 OK\r\n\r\n'
+                    okline = 'a'
                     if data.decode() == okline:
                         LINE = composeSipMsg('ACK', server_addr)
                         my_socket.send(bytes(LINE, 'utf-8'))
@@ -51,15 +72,14 @@ class handleXML(ContentHandler):
 
     def startElement(self, name, attrs):
 
-        attlist = []
+        attdict = {}
 
         if name != 'config':
             for attrib in tags[name]:
 
                 toAppend = attrs.get(attrib, '')
-                attlist.append(toAppend)
-                print(toAppend)
-                self.XML[name] = attlist
+                attdict[attrib] = toAppend
+                self.XML[name] = attdict
 
 
     def get_tags(self):
@@ -79,10 +99,11 @@ if __name__ == "__main__":
     except(FileNotFoundError, IndexError, ValueError):
         sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
-    #doClient(SERVER_ADDR, SIPMSG)
+
     parser = make_parser()
     cHandler = handleXML()
     parser.setContentHandler(cHandler)
     parser.parse(open(config_file))
     config_data = cHandler.get_tags()
     print(config_data)
+    doClient(config_data, sip_method)
